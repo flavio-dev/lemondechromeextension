d5933bb7857537026a73e1182eb7432b1cc27bc3
flavio-dev <trash_flavio@hotmail.fr>
2020-12-03 18:17:22 +0100

    one more case

--- script.js
+++ script.js
@@ -4,13 +4,19 @@
 
 'use strict';
 
-const articleHeightClass = 'article__content--restricted-media';
+const articleHeightMediaClass = 'article__content--restricted-media';
+const articleHeightClass = 'article__content--restricted';
 const paywallClass = 'paywall';
 const messageRegisterId = 'js-message-register';
 
+const articleMediaHeight = document.getElementsByClassName(articleHeightMediaClass)[0];
 const articleHeight = document.getElementsByClassName(articleHeightClass)[0];
 const paywall = document.getElementsByClassName(paywallClass)[0];
 const messageRegister = document.getElementById(messageRegisterId);
+
+if (articleMediaHeight) {
+  articleMediaHeight.style.height = '100%';
+}
 
 if (articleHeight) {
   articleHeight.style.height = '100%';
